72b7659395ed4884eefaa80ae652a30195930a1a
lkuca <120181604+lkuca@users.noreply.github.com>
2023-08-30 12:55:47 +0300

    Create andmete-lisamine.sql

---
+++ andmete-lisamine.sql
@@ -0,0 +1,50 @@
+insert into guest(first_name, last_name, member_since)
+values ('Alenka', 'Plenka', '2000-01-23');
+insert into guest(first_name, last_name, member_since)
+values('Kiril' ,'Blinchik','2000-02-12');
+insert into guest(first_name, last_name, member_since)
+values('Sasha','Durslakov','2000-01-10');
+insert into guest(first_name, last_name, member_since)
+values('Pasha', 'Opolovnik', '2000-02-09');
+insert into guest(first_name, last_name, member_since)
+values('Luca','Chihulja','2000-05-09');
+
+
+insert into room_type(description_,max_capacity)
+values('asdasdadasd',4);
+insert into room_type(description_,max_capacity)
+values('sfadsadafds',5);
+insert into room_type(description_,max_capacity)
+values('bjghjfigh',2);
+insert into room_type(description_,max_capacity)
+values('fdslkdj',1);
+insert into room_type(description_,max_capacity)
+values('fdjggjdfjg',3);
+
+insert into reservation(date_in, date_out, made_by)
+values('2000-01-10', '2000-02-10', 'china');
+insert into reservation(date_in, date_out, made_by)
+values('2000-03-10', '2000-04-10', 'Inglant');
+insert into reservation(date_in, date_out, made_by)
+values('2000-05-10', '2000-06-10','Canada');
+insert into reservation(date_in, date_out, made_by)
+values('2000-07-10', '2000-08-10', 'Estonia');
+insert into reservation(date_in, date_out, made_by)
+values('2000-09-10', '2000-010-10','France');
+
+insert into reserved_room(number_of_rooms, status_)
+values(1, 'active');
+insert into reserved_room(number_of_rooms, status_)
+values(2, 'active');
+insert into reserved_room(number_of_rooms, status_)
+values(3, 'deactive');
+insert into reserved_room(number_of_rooms, status_)
+values(4, 'deactive');
+insert into reserved_room(number_of_rooms, status_)
+values(5, 'active');
+
+
+
+
+
+

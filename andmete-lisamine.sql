e6ee6da85281935c2c2d80933dac0ff6ce11571d
lkuca <120181604+lkuca@users.noreply.github.com>
2023-09-03 22:32:16 +0300

    Update andmete-lisamine.sql

--- andmete-lisamine.sql
+++ andmete-lisamine.sql
@@ -43,6 +43,30 @@ values(4, 'deactive');
 insert into reserved_room(number_of_rooms, status_)
 values(5, 'active');
 
+insert into room(number,name,status)
+values(12,'k','active');
+insert into room(number,name,status)
+values(34,'l','active');
+insert into room(number,name,status)
+values(56,'v','deactive');
+insert into room(number,name,status)
+values(13,'c','active');
+insert into room(number,name,status)
+values(72,'m','deactive');
+
+
+insert into ocupied_room(check_in,check_out)
+values('2001-02-10','2001-02-20');
+insert into ocupied_room(check_in,check_out)
+values('2002-02-10','2002-02-12');
+insert into ocupied_room(check_in,check_out)
+values('2003-02-10','2003-02-1');
+insert into ocupied_room(check_in,check_out)
+values('2004-02-10','2004-02-21');
+insert into ocupied_room(check_in,check_out)
+values('2005-02-10','2005-02-28');
+
+
 
 
 

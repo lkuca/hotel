8d49fa7c07ba2293d765c3c9ee4df1283cca3085
lkuca <120181604+lkuca@users.noreply.github.com>
2023-09-27 13:13:55 +0300

    Update indeksid.sql

--- indeksid.sql
+++ indeksid.sql
@@ -1,158 +1,205 @@
+--Mitme avaldisega tabeliväärtusega funktsioonid
+
+Create Function fn_ILTVF_GetEmployees()
+Returns Table
+as
+Return (Select EmployeeKey,FirstName,Cast(BirthDate as Date) as DOB From DimEmployee);
+
+Select * from fn_ILTVF_GetEmployees();
+
+
+create Function fn_MSTVF_GetEmployees2()
+Returns @Table Table (Id int, Name varchar(20), DOB Date)
+as
+Begin
+Insert into @Table
+Select EmployeeKey, FirstName,CAST(BirthDate as Date)
+From DimEmployee
+Return
+End
+
+select * from fn_MSTVF_GetEmployees();
+
+Update fn_MSTVF_GetEmployees2() set Name='Sam 1' where id=1
+select * from fn_MSTVF_GetEmployees2();
+
+--Funktsiooniga seotud tähtsad kontseptsioonid
+
+create function fn_GetEmployeeNameBuild(@id int)
+Returns varchar(20)
+as
+Begin
+Return(Select FirstName from DimEmployee where EmployeeKey=@id)
+end
+
+select dbo.fn_GetEmployeeNameBuild(1);
+
+
+
+alter Function fn_GetEmployeeNameBuild(@Id int)
+returns varchar(20)
+with Encryption
+as
+Begin
+Return (Select FirstName from dbo.DimEmployee where EmployeeKey = @Id)
+End
+
+select dbo.fn_GetEmployeeNameBuild(1);
+
+
+drop table DimEmployee
+
+
+--Ajutised tabelid
+
+CREATE TABLE #PersonDetails(
+Id int PRIMARY KEY,
+Name VARCHAR(20))
+
+INSERT INTO #PersonDetails VALUES(1,'Artem')
+INSERT INTO #PersonDetails VALUES(2,'Matvei')
+INSERT INTO #PersonDetails VALUES(3,'Sasha')
+SELECT * FROM #PersonDetails
+
+SELECT name FROM  tempdb.sys.all_objects
+WHERE name LIKE '#PersonDetails%'
+
+
+
+CREATE PROCEDURE spCreateLocalTempTable
+AS
+BEGIN
+CREATE TABLE #PersonDetails(
+Id int PRIMARY KEY,
+Name VARCHAR(20))
+
+INSERT INTO #PersonDetails VALUES(1,'Artem')
+INSERT INTO #PersonDetails VALUES(2,'Matvei')
+INSERT INTO #PersonDetails VALUES(3,'Sasha')
+SELECT * FROM #PersonDetails
+END
+
+EXEC spCreateLocalTempTable
+
+CREATE TABLE ##EmployeeDetails(Id int, Name NVARCHAR(20))
+SELECT * FROM ##EmployeeDetails
+
+
+--Ineksid serveris
 
 select * from DimEmployee
+select * from DimEmployee where BaseRate > 35 and BaseRate <50
 
-Create TABLE [tblEmployee](
+
+
+create index IX_DimEmployee  where BaseRate > 35 and BaseRate < 50
+
+Create index IX_DimEmployee_BaseRate 
+on DimEmployee (BaseRate ASC)
+
+select top 10 * from DimEmployee order by IX_DimEmployee_BaseRate
+
+exec sys.sp_helpindex @Objname='DimEmployee'
+
+drop index DimEmployee.IX_DimEmployee_BaseRate
+
+--36. Klastreeritud ja mitte-klastreeritud indeksid
+
+select * from DimEmployee;
+
+execute sp_helpindex [tbEmployee];
+
+create table [tbEmployee]
+(
 [id] int Primary Key,
 [Name] nvarchar(50),
 [Salary] int,
 [Gender] nvarchar(10),
-[City] nvarchar(50))
+[City] nvarchar(10)
+)
 
-drop table [tblEmployee]
+insert into [tbEmployee] Values(3,'John',4500,'Male','New York')
+insert into [tbEmployee] Values(1,'Don',3400,'Male','London')
+insert into [tbEmployee] Values(4,'Gan',6500,'Female','Tokyo')
+insert into [tbEmployee] Values(5,'Pid',2100,'Female','Toronto')
+insert into [tbEmployee] Values(2,'Onas',2400,'Male','Sydney')
 
-execute sp_helpindex tblEmployee
+Select * from [tbEmployee]
 
-insert into tblEmployee Values(3,'John',4500,'Male','New York')
-insert into tblEmployee Values(1,'Sam',2500,'Male','London')
-insert into tblEmployee Values(4,'Sara',5500,'Female','Tokyo')
-insert into tblEmployee Values(2,'Pam',6500,'Female','Sydney')
-insert into tblEmployee Values(5,'Todd',3100,'Male','Toronto')
+create Clustered Index IX_tbEmployee_Name
+ON [tbEmployee](Name)
 
-select * from tblEmployee
+Drop index [tbEmployee].PK__tbEmploy__3213E83F65501ADA
 
-Create Clustered Index IX_tblEmployee_Gender_Salary
-On [tblEmployee]([Gender] DESC, [Salary] ASC)
-select * from [tblEmployee]
+create Clustered index IX_tblEmployee_Gender_Salary
+on tbEmployee(Gender desc, Salary ASC)
 
-Drop index [tblEmployee].PK__tblEmplo__3213E83FEC21E326
+Select * from tbEmployee
 
-create NonClustered Index IX_tblEmployee_Name
-on tblEmployee(Name)
+create NonClustered index IX_tbEmployee_Name
+on tbEmployee(Name)
 
+--37. Unikaalne ja mitte-unikaalne indeks
 
+create table [tbEmployee]
+(
+[id] int Primary Key,
+[Name] nvarchar(50),
+[Salary] int,
+[Gender] nvarchar(10),
+[City] nvarchar(10)
+)
 
-insert into tblEmployee values(1,'Mike','Sandoz',4500,'Male','New York')
-insert into tblEmployee Values(1,'John','Menco',2500,'Male','London')
+execute sp_helpindex [tbEmployee];
 
+insert into [tbEmployee] Values(1,'John',4500,'Male','New York')
+insert into [tbEmployee] Values(1,'Don',3400,'Male','London')
 
-Insert into tblEmployee Values(1,'Mike', 'Sandoz',4500,'Male','New York')
-Insert into tblEmployee Values(1,'John', 'Menco',2500,'Male','London')
+drop index tbEmployee.PK__tbEmploy__3213E83F55419F46
 
-create unique NonClustered Index UIX_tblEmployee_FirstName_LastName
-on [tblEmployee]([Name], [City])
+Create Unique NonClustered Index UIX_tblEmployee_Name_City
+on tbEmployee(Name desc, City ASC)
 
+ALTER TABLE tbEmployee 
+ADD CONSTRAINT UQ_tblEmployee_City 
+UNIQUE NONCLUSTERED (City)
 
+CREATE UNIQUE INDEX IX_tblEmployee_City
+ON tbEmployee(City)
+WITH IGNORE_DUP_KEY
 
-alter table [tblEmployee]
-add constraint uq_tblEmployee_City
-unique nonclustered([City])
+--38. Indeksi plussid ja miinused
 
-EXECUTE SP_HELPCONSTRAINT [tblEmployee]
+create table [tblEmployee]
+(
+[id] int primary key,
+[FirstName] nvarchar(50),
+[LastName] nvarchar(50),
+[Salary] int,
+[Gender] nvarchar(10),
+[City] nvarchar(50)
+)
 
-create unique index IX_tblEmployee_City
-on [tblEmployee]([City])
-with IGNORE_DUP_KEY
+insert into tblEmployee Values(1, 'Mike', 'Groshev', 5400, 'Male', 'New York')
+insert into tblEmployee Values(2, 'Luca', 'Kerekesha', 7600, 'Female', 'Kyiv')
+insert into tblEmployee Values(3, 'Max', 'Yanovich', 2300, 'Female', 'Moscow')
+insert into tblEmployee Values(4, 'Mona', 'Grozny', 1200, 'Male', 'Tokyo')
+insert into tblEmployee Values(5, 'Lisa', 'Veliky', 9100, 'Female', 'Las Vegas')
 
+select * from tblEmployee;
 
-create table tblEmployee(
-id int primary key,
-Name nvarchar(30),
-salary int,
-Gender nvarchar(10),
-DepartmentId int)
+create nonClustered index IX_tblEmployee_Salary
+on tblEmployee (Salary Asc)
 
-create table tblDepartment(
-DeptId int Primary KEy,
-DeptName nvarchar(20))
+select * from tblEmployee where Salary>4000 and Salary<8000
 
-drop table tblDepartment
-drop table tblEmployee
-insert into tblDepartment values (1,'IT')
-insert into tblDepartment values(2,'Payroll')
-insert into tblDepartment values (3,'HR')
-insert into tblDepartment values (4,'Admin')
+delete from tblEmployee where Salary=2500
+update tblEmployee Set Salary=9000 where Salary = 7500
 
-insert into tblEmployee values (1,'john', 5000, ' Male',3)
-insert into tblEmployee values(2,'Mike',3400,'MAle',2)
-insert into tblEmployee values (3,'Pam',6000,'Female',1)
-insert into tblEmployee values(4,'todd', 4800,'Male',4)
-insert into tblEmployee values(5,'sara', 3200,'Female',1)
-insert into tblEmployee values(6,'Ben', 4800,'Male',3)
+select * from tblEmployee order by Salary desc
 
-select * from tblEmployee
-
-select Id,Name,Salary,Gender,DeptName
+select Salary, count(Salary) as Total
 from tblEmployee
-join tblDepartment
-on tblEmployee.DepartmentId = tblDepartment.DeptId
-
-create view vWEmployeesByDepartment
-as
-select id,Name,Salary,Gender,DeptName
-from tblEmployee
-join tblDepartment
-on tblEmployee.DepartmentId = tblDepartment.DeptId
-
-select * from  vWEmployeesByDepartment
-
-create View vWITDepartment_Eployees
-as
-select Id,Name,Salary,Gender,DeptName
-from tblEmployee
-join tblDepartment
-on tblEmployee.DepartmentId = tblDepartment.DeptId
-where tblDepartment.DeptName = 'IT'
-
-Select * from vWITDepartment_Eployees
-
-create View vWEmpoyeesNonConfidentialData
-as
-Select Id,Name,Gender,DeptName
-from tblEmployee
-join tblDepartment
-on tblEmployee.DepartmentId = tblDepartment.DeptId
-
-select * from vWEmpoyeesNonConfidentialData
+Group by Salary
 
 
-create view vWEmployeesCountByDepartment
-as
-Select DeptName,COUNT(Id) as TotalEmployees
-from tblEmployee
-join tblDepartment
-on tblEmployee.DepartmentId = tblDepartment.DeptId
-Group By DeptName
-
-select * from vWEmployeesCountByDepartment
-
-
-create table tblEmployee(
-Id int Primary Key,
-Name nvarchar(30),
-Salary int,
-Gender nvarchar(10),
-DepartmentId int)
-
-
-insert into tblEmployee values (1,'John', 5000,'Male',3)
-insert into tblEmployee values(2,'Mike',3400,'Male',2)
-insert into tblEmployee values(3,'Pam',6000,'Female',1)
-insert into tblEmployee values(4,'todd', 4800,'Male',4)
-insert into tblEmployee values(5,'Sara',3200,'female',1)
-insert into tblEmployee values(6,'Ben',4800,'Male',3)
-
-create view vWEmployeesDataExceptSalary
-as
-Select Id,Name,Gender,DepartmentId
-from tblEmployee
-
-select * from vWEmployeesDataExceptSalary
-
-Update vWEmployeesDataExceptSalary
-set Name = 'Mikey' where Id = 2
-
-delete from vWEmployeesDataExceptSalary where Id = 2
-Insert into vWEmployeesDataExceptSalary values(2,'Mikey','Male',2)
-
-
-create table tble
